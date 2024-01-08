b3e251a1fc82f8eb9f82596b8d5aa3ce862f559a
john ontita <johnontita2000@gmail.com>
2024-01-08 12:26:50 +0300

    complete responsive design

---
+++ app.js
@@ -0,0 +1,8 @@
+var navLinks=document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right="0"  //style.right to display the menu
+}
+function hideMenu(){
+    navLinks.style.right="-200px"//to close the menu
+}
+console.log('johnwebdevloper')
